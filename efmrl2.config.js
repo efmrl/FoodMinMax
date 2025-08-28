51e0376f354f347a98e0ebaeb82705452233c52d
Sam Falkner <sam.falkner@gmail.com>
2025-08-28 11:43:26 -0600

    add efmrl config

---
+++ efmrl2.config.js
@@ -0,0 +1,11 @@
+{
+    "version": 1,
+    "efmrl": "FoodMinMax",
+    "canonURL": "https://efmrl-74206003-8435-11f0-8295-0242ac110002.efmrl.work",
+    "apiPrefix": "/.e",
+    "base_host": "efmrl.work",
+    "root_dir": "public",
+    "index_rewrite": [
+        "index.html"
+    ]
+}
